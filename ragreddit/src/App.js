f83942942ac7edfc82d369cd35480521235542d7
Aodj05 <aodj05@msn.com>
2022-07-21 15:23:32 -0400

    hooks, post state, Post list, render posts

--- ragreddit/src/App.js
+++ ragreddit/src/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import { Container, Flex, Spinner, VStack } from "@chakra-ui/core";
+import React, { useEffect, useState } from "react";
+import Post from "./components/post";
+import db from "./lib/firebase";
 
-function App() {
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    //post fetching
+
+    db.collection("posts")
+      .orderBy("createdAt", "desc")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setPosts(data);
+      });
+  }, []);
+
   return (
-    <div> RagReddit </div>
+    <>
+      <Container maxW="md" centerContent p={8}>
+        <VStack spacing={8} w="100%">
+          {posts.map((post) => (
+            <Post post={post} key={post.id} />
+          ))}
+        </Vstack>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
